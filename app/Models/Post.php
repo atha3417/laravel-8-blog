0df594155d73026e840a4bbfc24f0de68a232f76
Atha 3417 <Atha.3417@gmail.com>
2021-07-17 06:39:10 +0700

    using database

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,32 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class Post
+class Post extends Model
 {
-    private static $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Atha Tsaqif",
-            "body" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi, voluptatum. Ullam natus, inventore odit neque quibusdam provident impedit dolor ad sequi magni ipsam maiores aperiam soluta porro delectus, culpa quia. Commodi sit rem nobis placeat non. Corporis, obcaecati mollitia dolores facere magnam pariatur vero quaerat. Dicta debitis culpa deleniti blanditiis labore accusantium rerum. Tempora, id eaque quis fuga pariatur aspernatur sint eos deleniti qui mollitia inventore ducimus. Accusantium itaque nemo in natus eaque! Cum necessitatibus ea eveniet quaerat obcaecati eaque."
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Nafisa Zahra",
-            "body" => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et iure soluta ad sint perspiciatis quibusdam hic consequatur eaque aliquid obcaecati non corrupti sit at quis minus in, quasi corporis molestiae nesciunt repellat, exercitationem nihil iste nobis! Facere facilis eius quae voluptatibus eum maxime beatae nemo deserunt eligendi fugit, perspiciatis nostrum, voluptatem quasi libero explicabo. Corrupti tenetur, facere nihil accusamus quis fugit voluptates ab perferendis rerum ut eaque distinctio consequuntur numquam qui nostrum enim quos itaque suscipit. Iusto at eveniet nesciunt, officiis saepe odio consectetur sapiente possimus? Fuga corrupti tempora consectetur modi corporis iusto fugiat ea. Accusantium quasi fugiat dolorem deserunt."
-        ]
-    ];
+    use HasFactory;
 
-    public static function all()
-    {
-        return collect(self::$blog_posts);
-    }
+    // protected $fillable = ['title', 'excerpt', 'body'];
 
-    public static function find(String $slug)
-    {
-        $posts = static::all();
-        return $posts->firstWhere('slug', $slug);
-    }
+    protected $guarded = ['id'];
 }
